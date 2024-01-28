dddaef623e6aa5e0cb2eac6e2936776b96697004
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-01-28 22:30:01 +0000

    adding in the about me section and made it mobile responsive

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('header');
-    navbar.classList.toggle('sticky', window.scrollY > 0);
-});
+// window.addEventListener('scroll', () => {
+//     const navbar = document.querySelector('header');
+//     navbar.classList.toggle('sticky', window.scrollY > 0);
+// });
 
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
