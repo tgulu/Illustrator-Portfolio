f197fc502809b596b27e4e2b5a10572c767d3ce1
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-07-12 21:12:59 +0100

    add functionality for cart to add items and keep track of quantity

--- app.js
+++ app.js
@@ -67,15 +67,19 @@ graphic.addEventListener('click', function () {
 // })
 
 
+
+// Shop Cart Code
+
 let iconCart = document.querySelector('.icon-cart');
 let body = document.querySelector('section');
 let closeCart = document.querySelector('.close');
 let listProductHTML = document.querySelector('.list-product');
 let listCartHTML = document.querySelector('.list-cart');
+let iconCartSpan = document.querySelector('.icon-cart span');
 
 let listProducts = [];
 let carts = [];
-    
+
 iconCart.addEventListener('click', () => {
     body.classList.toggle('show-cart')
 })
@@ -85,8 +89,8 @@ closeCart.addEventListener('click', () => {
 
 const addDataToHTML = () => {
     listProductHTML.innerHTML = '';
-    if(listProducts.length > 0){
-        listProducts.forEach(product =>{
+    if (listProducts.length > 0) {
+        listProducts.forEach(product => {
             let newProduct = document.createElement('div');
             newProduct.classList.add('item');
             newProduct.dataset.id = product.id;
@@ -104,27 +108,70 @@ const addDataToHTML = () => {
 
 listProductHTML.addEventListener('click', (event) => {
     let positionClick = event.target;
-    if(positionClick.classList.contains('add-cart')){
+    if (positionClick.classList.contains('add-cart')) {
         let product_id = positionClick.parentElement.dataset.id;
         alert(product_id);
         addToCart(product_id)
     }
 })
 
-const addToCart = (product_id) =>{
+const addToCart = (product_id) => {
+    let postionThisProductInCart = carts.findIndex((value) => value.product_id == product_id);
+    if (carts.length <= 0) {
+        carts = [{
+            product_id: product_id,
+            quantity: 1
+        }]
+    } else if (postionThisProductInCart < 0) {
+        carts.push({
+            product_id: product_id,
+            quantity: 1
+        });
+    } else {
+        carts[postionThisProductInCart].quantity = carts[postionThisProductInCart].quantity + 1;
+    }
+    addCartToHTML();
+}
 
+const addCartToHTML = () => {
+    listCartHTML.innerHTML = '';
+    if (carts.length > 0) {
+        carts.forEach(cart => {
+            let newCart = document.createElement('div');
+            newCart.classList.add('item');
+            let positionProduct = listProducts.findIndex((value) => value.id == cart.product_id);
+            let info = listProducts[positionProduct];
+            newCart.innerHTML = `
+
+            <div class="image">
+                <img src="${info.image}" alt="${info.alt}>
+            </div>
+            <div class="name">
+                ${info.name} 
+            </div>
+            <div class="total-price">
+                 ${info.price} 
+            </div>
+            <div class="quantity">
+                <span class="minus"><</span>
+                <span>${cart.quantity}</span>
+                <span class="plus">></span>
+            </div>
+            `;
+            listCartHTML.appendChild(newCart);
+        })
+    }
 }
 
 const initApp = () => {
     fetch('/product.json')
-    .then(response => response.json())
-    .then(data => {
-        listProducts = data;
-        console.log(listProducts)
-        addDataToHTML()
-        
-    })
+        .then(response => response.json())
+        .then(data => {
+            listProducts = data;
+            console.log(listProducts)
+            addDataToHTML()
+
+        })
 
 }
 initApp();
-    
