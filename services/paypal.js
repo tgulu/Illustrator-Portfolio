0d7e56890046f8ed8c57366fe6ef6f43f13c5adf
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-08-09 11:53:06 +0100

    add comments to paypal.js

--- services/paypal.js
+++ services/paypal.js
@@ -1,14 +1,22 @@
-const axios = require('axios')
+// Import the axios library for making HTTP requests
+const axios = require('axios');
 
+// Asynchronous function to generate an access token from PayPal
 async function generateAcessToken() {
-    const respponse = await axois ({
-        url: process.env.PAYPAL_BASE_URL + '/v1/oauth2/token',
-        method: 'post',
+    
+    const response = await axios({
+        url: process.env.PAYPAL_BASE_URL + '/v1/oauth2/token', 
+        method: 'post', 
         data: 'grant_type=client_credentials',
         auth: {
-            username: process.env.PAYAPL_CLIENT_ID,
-            password:  process.env.PAYPAL_SECRET
+           
+            username: process.env.PAYAPL_CLIENT_ID, 
+            password: process.env.PAYPAL_SECRET
         }
-    })
-    
+    });
+
+    // Return the access token from the response data
+    return response.data.acess_token;
 }
+
+
