74253fdbdcd6c4c49eaa90907068b25882264920
Thabo Gulu <86728005+thabogulu@users.noreply.github.com>
2024-08-31 18:47:50 +0100

    fix navigation between graphic and sport pages

--- app.js
+++ app.js
@@ -16,39 +16,25 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     navMenu.classList.remove("active");
 }));
 
-// // *** Change Web page ***
+// *** Change Web page ***
 
-// // Redirecting to 'sport.html' when the element with id 'sports' is clicked
-// const sport = document.getElementById('sports');
-// sport.addEventListener('click', function () {
-//     window.location.href = 'sport.html';
-// });
-
-// // Redirecting to 'merchandise.html' when the element with id 'merchandise' is clicked
-// const merchandise = document.getElementById('merchandise');
-// merchandise.addEventListener('click', function () {
-//     window.location.href = 'merchandise.html';
-// });
-
-// // Redirecting to 'graphic.html' when the element with id 'graphic' is clicked
-// const graphic = document.getElementById('graphic');
-// graphic.addEventListener('click', function () {
-//     window.location.href = 'graphic.html';
-// });
-
-document.querySelectorAll('.image-container .image img').forEach(image => {
-    image.onclick = () => {
-        const popup = document.querySelector('.popup');
-        const popupImage = document.querySelector('.popup img');
-
-        popup.style.display = 'block';
-        popupImage.src = image.getAttribute('src');
-    };
+// Redirecting to 'sport.html' when the element with id 'sports' is clicked
+const sport = document.getElementById('sports');
+sport.addEventListener('click', function () {
+    window.location.href = 'sport.html';
 });
 
-document.querySelector('.popup span').onclick = () => {
-    document.querySelector('.popup').style.display = 'none';
-};
+// Redirecting to 'merchandise.html' when the element with id 'merchandise' is clicked
+const merchandise = document.getElementById('merchandise');
+merchandise.addEventListener('click', function () {
+    window.location.href = 'merchandise.html';
+});
+
+// Redirecting to 'graphic.html' when the element with id 'graphic' is clicked
+const graphic = document.getElementById('graphic');
+graphic.addEventListener('click', function () {
+    window.location.href = 'graphic.html';
+});
 
 
 
